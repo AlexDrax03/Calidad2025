dc00ddb2d5fe7092f5713dd54b382e71e0842b79
AlexDrax03 <1cizquierdochristian@gmail.com>
2023-11-24 01:23:11 -0500

    cambios

---
+++ src/main/resources/static/js/scripts.js
@@ -0,0 +1,24 @@
+/*!
+* Start Bootstrap - Business Casual v7.0.8 (https://startbootstrap.com/theme/business-casual)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
+*/
+// Highlights current date on contact page
+window.addEventListener('DOMContentLoaded', event => {
+    const listHoursArray = document.body.querySelectorAll('.list-hours li');
+    listHoursArray[new Date().getDay()].classList.add(('today'));
+})
+
+function enviarDatos(){
+    event.preventDefault();
+    
+    var user = document.getElementById("usuario").value;
+    var contra = document.getElementById("contra").value;
+    var b = 'bebida';
+    
+    
+    window.location.href = "http://localhost:8099/"+user+"/";
+   
+    
+}
+
